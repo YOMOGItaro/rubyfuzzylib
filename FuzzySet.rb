ba6cd50d2a65a2cc5a362ee3a17ff78cb6d5ee82
SHiguchi <higuchi@fs.se.uec.ac.jp>
2011-07-05 13:49:59 +0900

    ファジィセットに追加

--- FuzzySet.rb
+++ FuzzySet.rb
@@ -1,9 +1,9 @@
-
 class FuzzySet
 
   @name
   @membership_functions
   
+
   def initialize(name, membership_functions)
     @name = name
     @membership_functions = membership_functions
@@ -18,4 +18,18 @@ class FuzzySet
   def reference(label)
     @membership_functions[label]
   end
+end
+
+
+
+if __FILE__ == $0
+  require 'MembershipFunction.rb'
+
+  mfs = Hash.new
+  mfs["low"] = MembershipFunctionTrapezoid.new(  1, 50, 50,100)
+  mfs["mid"] = MembershipFunctionTrapezoid.new( 50,100,100,150)
+  mfs["top"] = MembershipFunctionTrapezoid.new(100,150,150,200)
+
+  fs = FuzzySet.new("baribari", mfs)
+  p fs.reference("low")
 end
